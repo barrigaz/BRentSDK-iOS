02844b495c983e1e85c0071e54b46fc39ef3094b
Danil <vahidov@gmail.com>
2023-06-30 18:53:47 +0200

    ios 15

--- BRentSDK.xcframework/ios-x86_64-simulator/BRentSDK.framework/Headers/BRentSDK.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-//! Project version number for BRentSDK.
-FOUNDATION_EXPORT double BRentSDKVersionNumber;
-
-//! Project version string for BRentSDK.
-FOUNDATION_EXPORT const unsigned char BRentSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BRentSDK/PublicHeader.h
